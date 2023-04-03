fe71d99c6813977e6d050a71cd3899c48e204e10
elwaagaa <elias.walter.waagaard@cern.ch>
2023-04-03 11:11:16 +0200

    Updated main analysis script for batch submission.

--- 003_PyIBS_Xsuite_treemaker/master_jobs/001_run_ibs/run_analysis.py
+++ 003_PyIBS_Xsuite_treemaker/master_jobs/001_run_ibs/run_analysis.py
@@ -1,5 +1,8 @@
-# IBS module from M. Zampetakis in https://github.com/MichZampetakis/IBS_for_Xsuite
-import tree_maker
+"""
+Main script to compare different IBS modes: analytical, kinetic, simple - combining with Xsuite tracking 
+IBS module from M. Zampetakis in https://github.com/MichZampetakis/IBS_for_Xsuite
+"""
+
 import sys
 import pathlib
 import json
@@ -7,25 +10,20 @@ import numpy as np
 import xobjects as xo
 import xtrack as xt
 import xpart as xp
-import matplotlib.pyplot as plt
 import pandas as pd
-from cpymad.madx import Madx
 from scipy.constants import e as qe
 from scipy.constants import m_p, m_e
 import yaml
-
-
 
 # Load simulation parameters
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-tree_maker.tag_json.tag_it(config['log_file'], 'started')  
-
 import sys
 sys.path.append(config['ibs_lib_path'])
 from lib.IBSfunctions import *
 
+# Initiate values from config file 
 xsuite_line = config['xsuite_line']
 energy=config['energy']
 nemitt_x = config["emit_x"]*1e-6
@@ -40,10 +38,17 @@ IBS_step = int(config['IBS_step'])
 Energy_loss = 0
 save_to = config['save_to']
 
-if config['modes'] == ['all']:
-  modes = ['analytical', 'kinetic', 'simple']
-else:
-  modes = config['modes']
+# Activate the different modes
+modes = []
+if config['mode_kinetic']: 
+    modes.append('kinetic')
+if config['mode_analytical']: 
+    modes.append('analytical')
+if config['mode_simple']: 
+    modes.append('simple')
+
+# Check that saving location exists 
+pathlib.Path(config['save_to']).mkdir(parents=True, exist_ok=True)
 ###########################################################
 
 ## Load xsuite line
@@ -65,7 +70,6 @@ context = xo.ContextCpu()         # For CPU
 ## Transfer lattice on context and compile tracking code
 tracker = xt.Tracker(_context=context, line=line,
                     extra_headers=['#define XTRACK_MULTIPOLE_NO_SYNRAD'])
-
 tracker.optimize_for_tracking()
 
 particles0 = xp.generate_matched_gaussian_bunch(
@@ -75,110 +79,163 @@ particles0 = xp.generate_matched_gaussian_bunch(
 
 tw = tracker.twiss(particle_ref = p0)
 
-# ----- Initialize IBS -----
+# ----- Initialize IBS object -----
 IBS = NagaitsevIBS()
 IBS.set_beam_parameters(particles0)
-#IBS.Npart = bunch_intensity
 IBS.set_optic_functions(tw)
 
-
+# Write used simulation parameters 
 sim_params = ''
-print()
-print("IBS class parameters:\n")
+print("\nIBS class parameters:\n")
 attributes = {attr: getattr(IBS, attr) for attr in dir(IBS) if not callable(getattr(IBS, attr)) and not attr.startswith('__')}
 for attr, value in attributes.items():
     print(f'{attr}: {value}')
     sim_params+=f'{attr}: {value}\n'
-sim_params+=f"modes: {modes}\n"
 print()
 
 with open("sim_params.txt", "w") as file:
   file.write(sim_params)
 
-
-#for mode in ['analytical', 'kinetic', 'simple']:
+###### ITERATE OVER THE CHOSEN MODES #######
 for mode in modes:
-  print(f"Model: {mode}")
-
-  particles = particles0.copy()
-
-  dt = 1./IBS.frev # consecutive turns/frev, used only for analytical, 
-  
+    
+    print(f"\nModel: {mode}\n")
+    particles = particles0.copy()
+    dt = 1./IBS.frev # consecutive turns/frev, used only for analytical, 
+    
     ## Initialize dictionaries
-  turn_by_turn = {}
-  
-  record_emit = ['eps_x', 'eps_y', 'sig_delta', 'bl']
-  for nn in record_emit:
-      turn_by_turn[nn] = np.zeros((n_turns), dtype = float)
+    turn_by_turn = {}
+    record_emit = ['eps_x', 'eps_y', 'epsn_x', 'epsn_y', 'sig_delta', 'bl']
+    for nn in record_emit:
+        turn_by_turn[nn] = np.zeros((n_turns), dtype = float)
+    
+    # --- Initialize first turn-by-turn data for all modes 
+    sig_x = np.std(particles.x[particles.state > 0])
+    sig_y = np.std(particles.y[particles.state > 0])
+    sig_delta = np.std(particles.delta[particles.state > 0])
+    turn_by_turn['bl'][0]        = np.std(particles.zeta[particles.state > 0])
+    turn_by_turn['sig_delta'][0] = sig_delta
+    turn_by_turn['eps_x'][0]     = (sig_x**2 - (tw['dx'][0] * sig_delta)**2) / tw['betx'][0]
+    turn_by_turn['eps_y'][0]     = sig_y**2 / tw['bety'][0] 
 
-  # --- Initialize 
-  sig_x = np.std(particles.x[particles.state > 0])
-  sig_y = np.std(particles.y[particles.state > 0])
-  sig_delta = np.std(particles.delta[particles.state > 0])
-  turn_by_turn['bl'][0]        = np.std(particles.zeta[particles.state > 0])
-  turn_by_turn['sig_delta'][0] = sig_delta
-  turn_by_turn['eps_x'][0]     = (sig_x**2 - (tw['dx'][0] * sig_delta)**2) / tw['betx'][0]
-  turn_by_turn['eps_y'][0]     = sig_y**2 / tw['bety'][0] 
+    # Initialize dictionaries for the integrals and growth rates
+    if mode == 'analytical':
+        turn_by_turn_analytical_integrals = {}
+        integral_keys = ['Ixx', 'Iyy', 'Ipp']
+        for nn in integral_keys:
+            turn_by_turn_analytical_integrals[nn] = np.zeros((n_turns), dtype = float)
+    
+    elif mode == 'kinetic':
+        turn_by_turn_kinetic_integrals = {}
+        integral_keys = ['kinTx', 'kinTy', 'kinTz']
+        for nn in integral_keys:
+            turn_by_turn_kinetic_integrals[nn] = np.zeros((n_turns), dtype = float)
+            
+    elif mode == 'simple':
+        turn_by_turn_simple_integrals = {}
+        integral_keys = ['Ixx', 'Iyy', 'Ipp']
+        for nn in integral_keys:
+            turn_by_turn_simple_integrals[nn] = np.zeros((n_turns), dtype = float)
 
-  for i in range(1, n_turns):
-      #import time
-      #start = time.time()
-  
-      print('Turn = ', i)
-      print('N_part = ',len(particles.x[particles.state > 0]))
-  
-      
-      if mode != 'analytical':
-        sig_x = np.std(particles.x[particles.state > 0])
-        sig_y = np.std(particles.y[particles.state > 0])
-        sig_delta = np.std(particles.delta[particles.state > 0])
-        turn_by_turn['bl'][i]        = np.std(particles.zeta[particles.state > 0])
-        turn_by_turn['sig_delta'][i] = sig_delta
-        turn_by_turn['eps_x'][i]     = (sig_x**2 - (tw['dx'][0] * sig_delta)**2) / tw['betx'][0]
-        turn_by_turn['eps_y'][i]     = sig_y**2 / tw['bety'][0] 
-      
-      if mode == 'analytical':
-        if (i % IBS_step == 0) or (i==1):
-             IBS.calculate_integrals(turn_by_turn['eps_x'][i-1],turn_by_turn['eps_y'][i-1],turn_by_turn['sig_delta'][i-1],turn_by_turn['bl'][i-1])
-        Emit_x, Emit_y, Sig_M = IBS.emit_evol(turn_by_turn['eps_x'][i-1],turn_by_turn['eps_y'][i-1],turn_by_turn['sig_delta'][i-1],turn_by_turn['bl'][i-1], dt)
+
+    # TRACK the particles over the given number of turns 
+    for i in range(1, n_turns):
+    
+        print('Turn = ', i)
+        print('N_part active = ',len(particles.x[particles.state > 0]))
+    
+        # For the non-analytical cases, extract beam properties from particle object 
+        if mode == 'kinetic' or mode == 'simple':
+            sig_x = np.std(particles.x[particles.state > 0])
+            sig_y = np.std(particles.y[particles.state > 0])
+            sig_delta = np.std(particles.delta[particles.state > 0])
+            turn_by_turn['bl'][i]        = np.std(particles.zeta[particles.state > 0])
+            turn_by_turn['sig_delta'][i] = sig_delta
+            turn_by_turn['eps_x'][i]     = (sig_x**2 - (tw['dx'][0] * sig_delta)**2) / tw['betx'][0]
+            turn_by_turn['eps_y'][i]     = sig_y**2 / tw['bety'][0] 
         
-        Sigma_E = Sig_M*IBS.betar**2
-        #BunchL = BunchLength(IBS.Circu, Harmonic_Num, IBS.EnTot, IBS.slip, 
-        #           Sigma_E, IBS.betar, RF_Voltage*1e-3, Energy_loss, IBS.Ncharg)
-        BunchL = ion_BunchLength(IBS.Circu, Harmonic_Num, IBS.EnTot, IBS.slip, 
-                   Sigma_E, IBS.betar, RF_Voltage*1e-3, Energy_loss, IBS.Ncharg)
+        # For the analytical case, calculate the integrals for each X turns and calculate theoretical emittance evolution
+        if mode == 'analytical':
+            
+            # Calculate the IBS integrals and add the kick 
+            if (i % IBS_step == 0) or (i==1):
+                 IBS.calculate_integrals(
+                     turn_by_turn['eps_x'][i-1],
+                     turn_by_turn['eps_y'][i-1],
+                     turn_by_turn['sig_delta'][i-1],
+                     turn_by_turn['bl'][i-1]
+                     )
+            Emit_x, Emit_y, Sig_M = IBS.emit_evol(
+                                                turn_by_turn['eps_x'][i-1],
+                                                turn_by_turn['eps_y'][i-1],
+                                                turn_by_turn['sig_delta'][i-1],
+                                                turn_by_turn['bl'][i-1], 
+                                                dt
+                                                  )
+            # Calculate energy spread and bunch length 
+            Sigma_E = Sig_M*IBS.betar**2
+            BunchL = ion_BunchLength(IBS.Circu, Harmonic_Num, IBS.EnTot, IBS.slip, 
+                       Sigma_E, IBS.betar, RF_Voltage*1e-3, Energy_loss, IBS.Ncharg)
+            
+            # Save the data 
+            turn_by_turn['bl'][i]        = BunchL
+            turn_by_turn['sig_delta'][i] = Sig_M
+            turn_by_turn['eps_x'][i]     = Emit_x
+            turn_by_turn['eps_y'][i]     = Emit_y
+          
+            turn_by_turn_analytical_integrals['Ixx'][i] = IBS.Ixx
+            turn_by_turn_analytical_integrals['Iyy'][i] = IBS.Iyy
+            turn_by_turn_analytical_integrals['Ipp'][i] = IBS.Ipp  
+
+        # Apply the kicks and save the integrals 
+        elif mode == "kinetic":
+            if (i % IBS_step == 0) or (i==1):
+                IBS.calculate_kinetic_coefficients(particles)
+            IBS.apply_kinetic_kick(particles)
+            print(f"Dx: {IBS.Dx}, Dy: {IBS.Dy}, Dz: {IBS.Dz}")
+            print(f"Fx: {IBS.Fx}, Fy: {IBS.Fy}, Fz: {IBS.Fz}")
+
+            # Check the growth rates 
+            turn_by_turn_kinetic_integrals['kinTx'][i] = IBS.Dx - IBS.Fx
+            turn_by_turn_kinetic_integrals['kinTy'][i] = IBS.Dy - IBS.Fy
+            turn_by_turn_kinetic_integrals['kinTz'][i] = IBS.Dz - IBS.Fz
+            
+        elif mode == "simple":
+            if (i % IBS_step == 0) or (i==1):
+                IBS.calculate_simple_kick(particles)
+            IBS.apply_simple_kick(particles)
+            
+            turn_by_turn_simple_integrals['Ixx'][i] = IBS.Ixx
+            turn_by_turn_simple_integrals['Iyy'][i] = IBS.Iyy
+            turn_by_turn_simple_integrals['Ipp'][i] = IBS.Ipp  
+            
+        # Track the particles
+        tracker.track(particles)
+
+    # Add the normalized emittances in one go
+    turn_by_turn['epsn_x'] = IBS.betar*IBS.gammar*turn_by_turn['eps_x']
+    turn_by_turn['epsn_y'] = IBS.betar*IBS.gammar*turn_by_turn['eps_y']
+
+    # Save the turn-by-turn_data with the integrals 
+    if mode == 'analytical':
+        # Merge dictionaries 
+        z1 = turn_by_turn | turn_by_turn_analytical_integrals
+        pd_analytical = pd.DataFrame(z1)
+        pd_analytical.index.name = 'Turn'
+        pathlib.Path(config['save_to']).mkdir(parents=True, exist_ok=True)
+        pd_analytical.to_parquet(f"{save_to}/xsuite_{mode}.parquet")
         
-        turn_by_turn['bl'][i]        = BunchL
-        turn_by_turn['sig_delta'][i] = Sig_M
-        turn_by_turn['eps_x'][i]     = Emit_x
-        turn_by_turn['eps_y'][i]     = Emit_y
-  
-      elif mode == "kinetic":
-        if (i % IBS_step == 0) or (i==1):
-             IBS.calculate_kinetic_coefficients(particles)
-        IBS.apply_kinetic_kick(particles)
-      elif mode == "simple":
-        if (i % IBS_step == 0) or (i==1):
-            IBS.calculate_simple_kick(particles)
-        IBS.apply_simple_kick(particles)
-  
-      tracker.track(particles)
-      #end = time.time()
-      #print(end - start)
-  
-  Emitt = []
-  Emitt.append(turn_by_turn['eps_x'])
-  Emitt.append(turn_by_turn['eps_x']*IBS.betar*IBS.gammar)
-  Emitt.append(turn_by_turn['eps_y'])
-  Emitt.append(turn_by_turn['eps_y']*IBS.betar*IBS.gammar)
-  Emitt.append(turn_by_turn['sig_delta'])
-  Emitt.append(turn_by_turn['bl'])
-  
-  df = pd.DataFrame(np.array(Emitt).T, columns=["eps_x", "epsn_x","eps_y", "epsn_y","sig_delta", "bl"])
-  df.index.name = 'Turn'
-
-  pathlib.Path(config['save_to']).mkdir(parents=True, exist_ok=True)
-
-  df.to_parquet(f"{save_to}/xsuite_{mode}.parquet")
-
-tree_maker.tag_json.tag_it(config['log_file'], 'completed')  
+    elif mode == 'kinetic':
+        z2 = turn_by_turn | turn_by_turn_kinetic_integrals
+        pd_kinetic = pd.DataFrame(z2)
+        pd_kinetic.index.name = 'Turn'
+        pathlib.Path(config['save_to']).mkdir(parents=True, exist_ok=True)
+        pd_kinetic.to_parquet(f"{save_to}/xsuite_{mode}.parquet")
+        
+    elif mode == 'simple':
+        z3 = turn_by_turn | turn_by_turn_simple_integrals
+        pd_simple = pd.DataFrame(z3)
+        pd_simple.index.name = 'Turn'
+        pathlib.Path(config['save_to']).mkdir(parents=True, exist_ok=True)
+        pd_simple.to_parquet(f"{save_to}/xsuite_{mode}.parquet")
+        
